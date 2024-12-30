d18aec9bd67daa75e764312d41b3cab1f4ac07c5
ykylejan <jkDellatan@mcm.edu.ph>
2024-12-30 14:18:34 +0800

    added dropdown functionality to the hamburger menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,22 @@
-import { Menu, ShoppingBasket } from "lucide-react";
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { Menu, ShoppingBasket, X } from "lucide-react";
 
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <div className="bg-[#334628] opacity-90 h-[86px] text-white flex items-center justify-between px-14  fixed top-0 left-0 w-full z-50">
+        <div className="bg-[#334628] opacity-90 h-[86px] text-white flex items-center justify-between px-14 fixed top-0 left-0 w-full z-50">
             <span className="text-3xl lg:text-4xl font-caveat cursor-pointer">MIN-JEE</span>
-            <Menu size={25} className="lg:hidden" />
+
+            <div className="lg:hidden" onClick={toggleMenu}>
+                {isMenuOpen ? <X size={25} /> : <Menu size={25} />}
+            </div>
 
             <div className="lg:flex space-x-7 font-inder hidden">
                 <span className="text-xl hover:text-[#778768] cursor-pointer">Shop</span>
@@ -13,6 +24,16 @@ const Navbar = () => {
                 <span className="text-xl hover:text-[#778768] cursor-pointer">Account</span>
                 <ShoppingBasket size={30} strokeWidth={1.5} className="hover:text-[#778768] cursor-pointer"/>
             </div>
+
+
+            {isMenuOpen && (
+                <div className="absolute top-[86px] left-0 w-full bg-[#334628] text-white flex flex-col items-center py-4 space-y-4 lg:hidden">
+                    <span className="text-xl hover:text-[#778768] cursor-pointer">Shop</span>
+                    <span className="text-xl hover:text-[#778768] cursor-pointer">About Us</span>
+                    <span className="text-xl hover:text-[#778768] cursor-pointer">Account</span>
+                    <ShoppingBasket size={30} strokeWidth={1.5} className="hover:text-[#778768] cursor-pointer"/>
+                </div>
+            )}
         </div>
     );
 };
