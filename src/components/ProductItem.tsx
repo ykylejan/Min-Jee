882e710351e92e2dee2ee966c5492e293956d8bc
ykylejan <jkDellatan@mcm.edu.ph>
2025-01-13 19:54:25 +0800

    feat: add temporary link route for ProductItem component

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const ProductItem = ({
@@ -6,18 +7,20 @@ const ProductItem = ({
     price = "PHP 8.00 - per day",
 }) => {
     return (
-        <div className="w-72 lg:w-64 hover:cursor-pointer relative overflow-hidden rounded-md">
-            <div className="relative">
-                <img
-                    src={image}
-                    alt="product_image"
-                    className="aspect-square rounded-md mb-3 transition-transform duration-300 hover:scale-105"
-                />
-                <div className="absolute inset-0 bg-white/30 opacity-0 transition-opacity duration-300 hover:opacity-100 rounded-md"></div>
+        <Link href="/product-item">
+            <div className="w-72 lg:w-64 hover:cursor-pointer relative overflow-hidden rounded-md">
+                <div className="relative">
+                    <img
+                        src={image}
+                        alt="product_image"
+                        className="aspect-square rounded-md mb-3 transition-transform duration-300 hover:scale-105"
+                    />
+                    <div className="absolute inset-0 bg-white/30 opacity-0 transition-opacity duration-300 hover:opacity-100 rounded-md"></div>
+                </div>
+                <h1 className="text-xl font-afacad_medium">{name}</h1>
+                <h1 className="text-md font-afacad">{price}</h1>
             </div>
-            <h1 className="text-xl font-afacad_medium">{name}</h1>
-            <h1 className="text-md font-afacad">{price}</h1>
-        </div>
+        </Link>
     );
 };
 
