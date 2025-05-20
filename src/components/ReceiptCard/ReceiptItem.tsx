ecbec0d0f1b573fa2d153eb3292e129f0312c7fe
ranielART <ranielmontebon24@gmail.com>
2025-05-21 02:41:20 +0800

    Event booking customer side receipt is successfully integrated

--- src/components/ReceiptCard/ReceiptItem.tsx
+++ src/components/ReceiptCard/ReceiptItem.tsx
@@ -1,31 +1,44 @@
-import { images } from "@/constants";
 import React from "react";
 
-const ReceiptItem = () => {
-    return (
-        <>
-            <div className="flex py-6">
-                <img
-                    src={images.halfsizedFoodWarmer.src}
-                    alt="product"
-                    className="h-[103px] aspect-square rounded-md object-cover"
-                />
-                <div className="w-full pl-4">
-                    <div className="flex justify-between w-full font-afacad_medium">
-                        <h1 className="text-2xl font-afacad_semibold">
-                            Half-sized Food Warmer
-                        </h1>
-                        <h1 className="text-xl">PHP 10.00</h1>
-                    </div>
-                    <div className="text-[#6B7280] flex flex-col font-afacad space-y-5">
-                        <h1 className="text-lg">Rental</h1>
-                        <h1 className="text-lg">Qty 1</h1>
-                    </div>
-                </div>
-            </div>
-            <hr />
-        </>
-    );
+interface ReceiptItemProps {
+  img?: string;
+  name?: string;
+  price?: number | string;
+  category?: string;
+  quantity?: number | string;
+}
+
+const ReceiptItem: React.FC<ReceiptItemProps> = ({
+  img,
+  name,
+  price,
+  category,
+  quantity,
+}) => {
+  return (
+    <>
+      <div className="flex py-6">
+        <img
+          src={img}
+          alt="product"
+          className="h-[103px] aspect-square rounded-md object-cover"
+        />
+        <div className="w-full pl-4">
+          <div className="flex justify-between w-full font-afacad_medium">
+            <h1 className="text-2xl font-afacad_semibold">{name}</h1>
+            <h1 className="text-xl">
+              PHP {price ? Number(price).toFixed(2) : "0.00"}
+            </h1>
+          </div>
+          <div className="text-[#6B7280] flex flex-col font-afacad space-y-5">
+            <h1 className="text-lg">{category}</h1>
+            <h1 className="text-lg">Qty {quantity || 1}</h1>
+          </div>
+        </div>
+      </div>
+      <hr />
+    </>
+  );
 };
 
 export default ReceiptItem;
